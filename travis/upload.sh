340a44b21ff4ce379ab86b783d343d77e990b6f2
Restyled.io <commits@restyled.io>
2021-02-07 21:59:40 +0000

    Restyled by shfmt

--- travis/upload.sh
+++ travis/upload.sh
@@ -3,12 +3,12 @@ echo Decrypting id_rsa...
 mkdir -p sshconfig
 
 if [ ! -z "$gh_rsa_key" ]; then
-    openssl aes-256-cbc -K "$gh_rsa_key" -iv "$gh_rsa_iv" -in .github/id_rsa.enc -out sshconfig/id_rsa -d || exit 1
+  openssl aes-256-cbc -K "$gh_rsa_key" -iv "$gh_rsa_iv" -in .github/id_rsa.enc -out sshconfig/id_rsa -d || exit 1
 elif [ ! -z "$encrypted_b1899526f957_key" ]; then
-    openssl aes-256-cbc -K "$encrypted_b1899526f957_key" -iv "$encrypted_b1899526f957_iv" -in travis/id_rsa.enc -out sshconfig/id_rsa -d || exit 1
+  openssl aes-256-cbc -K "$encrypted_b1899526f957_key" -iv "$encrypted_b1899526f957_iv" -in travis/id_rsa.enc -out sshconfig/id_rsa -d || exit 1
 else
-    echo "SSH key is not available, upload cancelled"
-    exit 0
+  echo "SSH key is not available, upload cancelled"
+  exit 0
 fi
 
 eval "$(ssh-agent -s)"
@@ -17,32 +17,32 @@ ssh-add sshconfig/id_rsa || exit 1
 
 SSHOPTS="ssh -o StrictHostKeyChecking=no"
 
-VERSION=`tr -d '\r' < PORTABLE_VERSION`
+VERSION=$(tr -d '\r' <PORTABLE_VERSION)
 
 if [ ! -z "$GITHUB_REF" ]; then
-    TRAVIS_BRANCH=${GITHUB_REF#"refs/heads/"}
-    echo "Ref: $GITHUB_REF"
-    echo "Branch: $TRAVIS_BRANCH"
+  TRAVIS_BRANCH=${GITHUB_REF#"refs/heads/"}
+  echo "Ref: $GITHUB_REF"
+  echo "Branch: $TRAVIS_BRANCH"
 fi
 
 case "$TRAVIS_OS_NAME" in
-    linux)
-        echo Uploading linux artifacts...
-        rsync -e "$SSHOPTS" deadbeef-*.tar.bz2 waker,deadbeef@frs.sourceforge.net:/home/frs/project/d/de/deadbeef/travis/linux/"$TRAVIS_BRANCH"/ || exit 1
-        rsync -e "$SSHOPTS" portable_out/build/*.tar.bz2 waker,deadbeef@frs.sourceforge.net:/home/frs/project/d/de/deadbeef/travis/linux/"$TRAVIS_BRANCH"/ || exit 1
-        rsync -e "$SSHOPTS" package_out/x86_64/debian/*.deb waker,deadbeef@frs.sourceforge.net:/home/frs/project/d/de/deadbeef/travis/linux/"$TRAVIS_BRANCH"/ || exit 1
-        rsync -e "$SSHOPTS" package_out/x86_64/arch/*.pkg.tar.xz waker,deadbeef@frs.sourceforge.net:/home/frs/project/d/de/deadbeef/travis/linux/"$TRAVIS_BRANCH"/ || exit 1
+  linux)
+    echo Uploading linux artifacts...
+    rsync -e "$SSHOPTS" deadbeef-*.tar.bz2 waker,deadbeef@frs.sourceforge.net:/home/frs/project/d/de/deadbeef/travis/linux/"$TRAVIS_BRANCH"/ || exit 1
+    rsync -e "$SSHOPTS" portable_out/build/*.tar.bz2 waker,deadbeef@frs.sourceforge.net:/home/frs/project/d/de/deadbeef/travis/linux/"$TRAVIS_BRANCH"/ || exit 1
+    rsync -e "$SSHOPTS" package_out/x86_64/debian/*.deb waker,deadbeef@frs.sourceforge.net:/home/frs/project/d/de/deadbeef/travis/linux/"$TRAVIS_BRANCH"/ || exit 1
+    rsync -e "$SSHOPTS" package_out/x86_64/arch/*.pkg.tar.xz waker,deadbeef@frs.sourceforge.net:/home/frs/project/d/de/deadbeef/travis/linux/"$TRAVIS_BRANCH"/ || exit 1
     ;;
-    osx)
-        echo Uploading mac artifacts...
-        rsync -e "$SSHOPTS" osx/build/Release/deadbeef-"$VERSION"-osx-x86_64.zip waker,deadbeef@frs.sourceforge.net:/home/frs/project/d/de/deadbeef/travis/osx/"$TRAVIS_BRANCH"/ || exit 1
+  osx)
+    echo Uploading mac artifacts...
+    rsync -e "$SSHOPTS" osx/build/Release/deadbeef-"$VERSION"-osx-x86_64.zip waker,deadbeef@frs.sourceforge.net:/home/frs/project/d/de/deadbeef/travis/osx/"$TRAVIS_BRANCH"/ || exit 1
     ;;
-    windows)
-        echo Uploading windows artifacts...
-        rsync -e "$SSHOPTS" bin/deadbeef-"$VERSION"-windows-x86_64.zip waker,deadbeef@frs.sourceforge.net:/home/frs/project/d/de/deadbeef/travis/windows/"$TRAVIS_BRANCH"/ || exit 1
-        rsync -e "$SSHOPTS" bin/deadbeef-"$VERSION"-windows-x86_64_DEBUG.zip waker,deadbeef@frs.sourceforge.net:/home/frs/project/d/de/deadbeef/travis/windows/"$TRAVIS_BRANCH"/ || exit 1
-        rsync -e "$SSHOPTS" bin/deadbeef-"$VERSION"-windows-x86_64.exe waker,deadbeef@frs.sourceforge.net:/home/frs/project/d/de/deadbeef/travis/windows/"$TRAVIS_BRANCH"/ || exit 1
-        rsync -e "$SSHOPTS" bin/deadbeef-"$VERSION"-windows-x86_64_DEBUG.exe waker,deadbeef@frs.sourceforge.net:/home/frs/project/d/de/deadbeef/travis/windows/"$TRAVIS_BRANCH"/ || exit 1
-        taskkill //IM ssh-agent.exe //F
+  windows)
+    echo Uploading windows artifacts...
+    rsync -e "$SSHOPTS" bin/deadbeef-"$VERSION"-windows-x86_64.zip waker,deadbeef@frs.sourceforge.net:/home/frs/project/d/de/deadbeef/travis/windows/"$TRAVIS_BRANCH"/ || exit 1
+    rsync -e "$SSHOPTS" bin/deadbeef-"$VERSION"-windows-x86_64_DEBUG.zip waker,deadbeef@frs.sourceforge.net:/home/frs/project/d/de/deadbeef/travis/windows/"$TRAVIS_BRANCH"/ || exit 1
+    rsync -e "$SSHOPTS" bin/deadbeef-"$VERSION"-windows-x86_64.exe waker,deadbeef@frs.sourceforge.net:/home/frs/project/d/de/deadbeef/travis/windows/"$TRAVIS_BRANCH"/ || exit 1
+    rsync -e "$SSHOPTS" bin/deadbeef-"$VERSION"-windows-x86_64_DEBUG.exe waker,deadbeef@frs.sourceforge.net:/home/frs/project/d/de/deadbeef/travis/windows/"$TRAVIS_BRANCH"/ || exit 1
+    taskkill //IM ssh-agent.exe //F
     ;;
 esac
